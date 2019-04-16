c83723f3d96e403e2a36a654d77350fdbd4fd128
Alex Borst <butler.borst@gmail.com>
2019-04-16 21:52:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,13 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads => {:pieces => 4}, 
-	  :name => {}
+ 		:railroads=>
+		{
+		:pieces=>4,
+		:names=>{},
+		:rent_in_dollars=>{}
+		}
 	}
-	  
 
 end
 
